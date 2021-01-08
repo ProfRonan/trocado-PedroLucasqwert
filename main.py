9cd3b193ba219ce22c49791784f8facd4bc4a54b
Pedro  Lucas <pedrolucas@alu.ufc.br>
2021-01-08 16:33:28 +0000

    terminei finalmente

--- main.py
+++ main.py
@@ -1,9 +1,46 @@
 """Arquivo principal que será interpretado pelo interpretador."""
+def entrada():
+   global valor
+   valor = float(input("Digite um valor a ser trocado até R$10:\n"))
+   teste_valor()
+def teste_valor():
+  if valor > 10:
+    print(f"Valor não valido")
+  else:
+   teste_t()
+def teste_t():
+      global valor
+      valor = valor * 100
+      print("São necessárias:")
+      if valor // 100 > 0:
+       print(f"moedas de R$ 1.00: {valor//100:.0f}" )
+      valor = valor%100
+      if valor // 50 > 0:
+       print(f"moedas de R$ 0.50: {valor//50:.0f}" )  
+      valor = valor%50
+      if valor // 25 > 0:
+       print(f"moedas de R$ 0.25: {valor//25:.0f}" )  
+      valor = valor%25
+      if valor // 10 > 0:
+       print(f"moedas de R$ 0.10: {valor//10:.0f}" )  
+      valor = valor%10      
+      if valor // 5 > 0:
+       print(f"moedas de R$ 0.05: {valor//5:.0f}" )   
+      valor = valor%5
+      if valor // 1 > 0:
+       print(f"moedas de R$ 0.01: {valor//1:.0f}" )   
+      valor = valor%1
+
+
 
 
 def main():
     """Função principal que será rodada quando o script for passado para o interpretador."""
-    # COLOQUE SEU CÓDIGO AQUI
+    
+    entrada()
+    
+    
+    
 
 
 if __name__ == '__main__':
